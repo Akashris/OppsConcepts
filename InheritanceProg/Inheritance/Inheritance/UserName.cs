81bead17b536e0e1f76f279f8601ce2bab1a0d42
Akashris <akashritharan@gmail.com>
2019-12-13 14:33:28 +0530

    First Upload

---
+++ InheritanceProg/Inheritance/Inheritance/UserName.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+
+
+    public class UserName
+    {
+        public string Name;
+        public void GetName()
+        {
+            Console.WriteLine("Name: {0}", Name);
+        }
+    }
+
+
+    public class Address : UserName
+    {
+        public string Location;
+        public void GetLocation()
+        {
+            Console.WriteLine("Location: {0}", Location);
+        }
+    }
+
+
+    public class UserAge : Address
+    {
+        public int Age;
+        public void GetAge()
+        {
+            Console.WriteLine("Age: {0}", Age);
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            UserAge obj = new UserAge();
+            obj.Name = "Akash Shritharan";
+            obj.Location = "Chennai";
+            obj.Age = 23;
+            obj.GetName();
+            obj.GetLocation();
+            obj.GetAge();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PolymorphismProg/Polymorphism/Polymorphism/Shape.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Shapes
+    {
+        public virtual void draw()
+        {
+            Console.WriteLine("Formula And Example");
+        }
+    }
+    public class Square : Shapes
+    {
+        public override void draw()
+        {
+            Console.WriteLine("\nSquare = Side * Side");
+            int side = 5, area;
+            Console.WriteLine("For a square of side {0} mts", side);
+            area = side * side;
+            Console.WriteLine("Area is {0} sqr mts", area);
+        }
+    }
+    public class Rectangle : Shapes
+    {
+        public override void draw()
+        {
+            Console.WriteLine("\nRectangle = Length * Breadth");
+            int length = 4, breadth = 6, area;
+            Console.WriteLine("For a rectangle of length {0}mts and {1}mts", length, breadth);
+            area = length * breadth;
+            Console.WriteLine("Area is {0} sqr mts", area);
+        }
+    }
+    public class Circle : Shapes
+    {
+        public override void draw()
+        {
+            Console.WriteLine("\nCircle = 3.14 * Radius * Radius");
+            double radius = 7;
+            double area;
+            Console.WriteLine("For a circle of radius {0}mts", radius);
+            area = 3.14 * radius * radius;
+            Console.WriteLine("Area is {0} sqr mts", area);
+        }
+    }
+
+    public class TestPolymorphism
+    {
+        public static void Main(string[] args)
+        {
+            Shapes obj;
+            obj = new Shapes();
+            obj.draw();
+            obj = new Square();
+            obj.draw();
+            obj = new Rectangle();
+            obj.draw();
+            obj = new Circle();
+            obj.draw();
+            Console.ReadKey();
+        }
+    }
+}
